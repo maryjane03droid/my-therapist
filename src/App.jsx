71c69d39052c60c4ef16dafd06396f7ddbdcf322
Maryjane <kuthiemaryjane@gmail.com>
2026-04-16 08:59:48 +0300

    fix bugs for versel deployement

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from "react-router-dom";
-import Layout from "./components/Layout";
+import Layout from "./components/layout";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 import Home from "./pages/home";
